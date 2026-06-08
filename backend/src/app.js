3f40d45d41a292d01ad4477ca694186bbb9c03a2
Tushar <tusharrathory60@gmail.com>
2026-06-08 20:09:38 +0530

    second commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ const app = express()
 const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://localhost:5173',
+  'https://t-drive-8c6p.onrender.com/',
   ...(process.env.CLIENT_ORIGIN ? process.env.CLIENT_ORIGIN.split(',') : []),
 ]
 
